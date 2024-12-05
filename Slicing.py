8709bdc8e25f0f2a9edd7038116553863d59a297
THSayeb <desktop499@gmail.com>
2024-12-05 23:10:03 +0600

    Slicing

---
+++ Slicing.py
@@ -0,0 +1,7 @@
+print("Hello")
+print("Here you can observe the opposite word of your input")
+
+words = input("______________________ \n  Give me a word")
+wordrev = words[::-1]
+
+print( "The new word is ", wordrev)
