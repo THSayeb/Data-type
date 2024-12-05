acfef7f8980d49624adae35a28ba28288fba5f72
THSayeb <desktop499@gmail.com>
2024-12-05 21:23:37 +0600

    Data type

---
+++ datatype.py
@@ -0,0 +1,11 @@
+str = "Hello world"
+print( str, " is a ",  type(str))
+
+int = 34
+print( int, " is an ", type(int))
+
+float = 4.23
+print( float, " is a ", type(float))
+
+bool = False
+print( bool, " is a ", type(bool))
